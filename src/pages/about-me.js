e70927ad1da834dab400e91e2a889388d6d7406a
Kok Ann <kokann.ng@pixl8.co.uk>
2019-07-13 15:00:43 +0800

    Use graphql inside gatsby site

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const AboutMePage = () => ( <div>
+const AboutMePage = ({data}) => ( <div>
             <h1>Im a web developer</h1>
             <p>trying to learn gatsby js</p>
+            <ul>
+                {data.site.siteMetadata.keywords.map((keyword, index) => (
+                    <li key={index}>{keyword}</li>
+                ))}
+            </ul>
         </div> )
+
+
+export const pageQuery = graphql`
+{
+    site {
+        siteMetadata {
+            title
+            keywords
+            description
+        }
+    }
+}
+`
 
 export default AboutMePage;
